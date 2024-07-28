88a74a732d4ade478c35d8ecafe5ba8021bab779
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-28 15:48:20 -0500

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -4,44 +4,46 @@
  * ss_cd - Changes the current working directory.
  * @cmds: The command to be executed.
  *
- * Returns: 1 on success, -1 on failure.
+ * Return: 1 on success, -1 on failure.
  */
-int ss_cd(const char *cmds); {
-    if (!cmds || !cmds[1]) {
+int ss_cd(const char *cmds)
+{
+    if (!cmds || !cmds[1])
+    {
         fprintf(stderr, "ss: expected argument to \"cd\"\n");
-        return -1;
+        return (-1);
     }
-    if (chdir(cmds[1]) == -1) {
+    if (chdir(cmds[1]) == -1)
+    {
         perror("ss");
-        return -1;
+        return (-1);
     }
-    return 1;
+    return (1);
 }
 
 /**
  * ss_exit - Exits the shell.
- *
- * No return value.
  */
-void ss_exit(void); {
-    // Exit the shell gracefully.
+void ss_exit(void)
+{
     exit(EXIT_SUCCESS);
 }
 
 /**
  * ss_help - Lists the built-in functions of the shell.
- *
- * No return value.
  */
-void ss_help(void); {
+void ss_help(void)
+{
     int i;
     const char *builtins[] = {"ss_cd", "ss_exit", "ss_help"};
-    printf("Welcome to the ATLAS Simple Shell!;\n");
-    printf("a group project made by Max and James\n");
-    printf("Here are the built in features.\n");
+
+    printf("Welcome to the ATLAS Simple Shell!\n");
+    printf("A group project made by Max and James\n");
+    printf("Here are the built-in features:\n");
     printf("Type the program names and arguments,\n");
     printf("Press the 'enter' key to execute your command!\n");
-    for (i = 0; i < sizeof(builtins)/sizeof(builtins[0]); i++) {
+    for (i = 0; i < sizeof(builtins) / sizeof(builtins[0]); i++)
+    {
         printf("%s\n", builtins[i]);
     }
     printf("If you need more information, feel free to use the 'man' command\n");
