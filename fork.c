ddc60fb3d0c5703f6d65a01451d7f495f05343a3
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-28 15:35:22 -0500

    Update fork.c

--- fork.c
+++ fork.c
@@ -10,32 +10,35 @@
  * @command: The command to execute.
  * @full_path: Full path to the command.
  *
- * Returns: Nothing.
+ * Description: This function forks a new process and executes a command using execve().
+ *              It waits for the child process to finish executing the command.
+ *
+ * Return: void
  */
 void fork_exec(char **command, char *full_path)
 {
-	pid_t pid = fork();
+    pid_t pid = fork();
 
-	if (pid == -1)
-	{
-		perror("Fork failed");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		if (execve(full_path, command, NULL) == -1)
-		{
-			perror("Execve failed");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		int status;
-		if (waitpid(pid, &status, 0) == -1)
-		{
-			perror("Waitpid failed");
-			exit(EXIT_FAILURE);
-		}
-	}
+    if (pid == -1)
+    {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        if (execve(full_path, command, NULL) == -1)
+        {
+            perror("Execve failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        int status;
+        if (waitpid(pid, &status, 0) == -1)
+        {
+            perror("Waitpid failed");
+            exit(EXIT_FAILURE);
+        }
+    }
 }
