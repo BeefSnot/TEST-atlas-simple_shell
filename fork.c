aef57410891be3f8729815b57fe4c62e882c5921
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-28 15:26:46 -0500

    Update fork.c

--- fork.c
+++ fork.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "shell.h"
 
 /**
@@ -9,24 +14,28 @@
  */
 void fork_exec(char **command, char *full_path)
 {
-    pid_t pid = fork();
+	pid_t pid = fork();
 
-    if (pid == -1)
-    {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        if (execve(full_path, command, NULL) == -1)
-        {
-            perror("Execve failed");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        int status;
-        waitpid(pid, &status, 0);
-    }
+	if (pid == -1)
+	{
+		perror("Fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(full_path, command, NULL) == -1)
+		{
+			perror("Execve failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		int status;
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("Waitpid failed");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
