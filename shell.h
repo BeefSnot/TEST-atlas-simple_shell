0ce0a6ea5691ce991fa8370c9a8de24346f870e5
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-29 16:46:26 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -21,8 +21,8 @@ char **prompt(void);
 void fork_exec(char **command, char *full_path);
 void find_path(char **cmds);
 int ss_cd(const char *cmds);
-void ss_exit(void)
-void ss_help(void)
+void ss_exit(void);
+void ss_help(void);
 void findenv(char **cmds);
 
 #endif
