3675643d2ab757f8a41438b529b99a0648852465
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-29 16:50:49 -0500

    Update env.c
    Testing This

--- env.c
+++ env.c
@@ -17,57 +17,63 @@
 void find_env(char **cmds)
 {
     char *path_env = getenv("PATH");
+    char *path_copy;
+    char *path_dir;
+    char *full_path;
+    pid_t pid;
+    int status;
+
     if (!path_env)
-        {
+    {
         fprintf(stderr, "Environment variable PATH is not set.\n");
         return;
-        }
+    }
 
-    char *path_copy = strdup(path_env);
+    path_copy = strdup(path_env);
     if (!path_copy)
-        {
+    {
         perror("Failed to duplicate PATH environment variable.");
         return;
-        }
+    }
 
-    char *path_dir = strtok(path_copy, ":");
+    path_dir = strtok(path_copy, ":");
     while (path_dir != NULL)
-        {
-        char *full_path = malloc(strlen(path_dir) + strlen(cmds[0]) + 2);
+    {
+        full_path = malloc(strlen(path_dir) + strlen(cmds[0]) + 2);
         if (!full_path)
-            {
+        {
             perror("Memory allocation failed");
             free(path_copy);
             return;
-            }
+        }
 
         strcpy(full_path, path_dir);
         strcat(full_path, "/");
         strcat(full_path, cmds[0]);
 
-        pid_t pid = fork();
+        pid = fork();
         if (pid == -1)
-            {
+        {
             perror("Failed to fork");
             free(full_path);
             continue;
-            }
+        }
         else if (pid == 0)
-            {
+        {
             if (execvp(cmds[0], cmds) == -1)
-                {
-                perror("Failed to execute command");
-                }
-            exit(EXIT_FAILURE);
-            }
-        else
             {
-            int status;
-            waitpid(pid, &status, 0);
+                perror("Failed to execute command");
             }
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            waitpid(pid, &status, 0);
+        }
 
         free(full_path);
         path_dir = strtok(NULL, ":");
-        }
+    }
+
     free(path_copy);
 }
