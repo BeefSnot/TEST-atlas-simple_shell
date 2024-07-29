3e1ca6a084479587dfa5bb46cf1ae31e1c3e6533
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-29 14:16:29 -0500

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -17,29 +17,29 @@ int ss_cd(const char *cmds)
     {
         perror("ss");
         return (-1);
+        }
+        return (1);
     }
-    return (1);
-}
 
-/**
- * ss_exit - Exits the shell.
- */
-void ss_exit(void)
-{
-    exit(EXIT_SUCCESS);
-}
+    /**
+     * ss_exit - Exits the shell.
+     */
+    void ss_exit(void)
+    {
+        exit(EXIT_SUCCESS);
+    }
 
-/**
- * ss_help - Lists the built-in functions of the shell.
- */
-void ss_help(void)
-{
-    int i;
-    const char *builtins[] = {"ss_cd", "ss_exit", "ss_help"};
+    /**
+     * ss_help - Lists the built-in functions of the shell.
+     */
+    void ss_help(void)
+    {
+        int i;
+        const char *builtins[] = {"ss_cd", "ss_exit", "ss_help"};
 
-    printf("Welcome to the ATLAS Simple Shell!\n");
-    printf("A group project made by Max and James\n");
-    printf("Here are the built-in features:\n");
+        printf("Welcome to the ATLAS Simple Shell!\n");
+        printf("A group project made by Max and James\n");
+        printf("Here are the built-in features:\n");
     printf("Type the program names and arguments,\n");
     printf("Press the 'enter' key to execute your command!\n");
     for (i = 0; i < sizeof(builtins) / sizeof(builtins[0]); i++)
@@ -49,3 +49,4 @@ void ss_help(void)
     printf("If you need more information, feel free to use the 'man' command\n");
     printf("to view the manual for the shell!\n");
 }
+
