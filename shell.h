d58d8dfc9534e0b8716536d88eded0593c125182
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-29 16:40:17 -0500

    Update shell.h
    Added semicolon

--- shell.h
+++ shell.h
@@ -20,7 +20,7 @@ char **parse(const char *line);
 char **prompt(void);
 void fork_exec(char **command, char *full_path);
 void find_path(char **cmds);
-int ss_cd(const char *cmds)
+int ss_cd(const char *cmds);
 void ss_exit(char **args);
 void ss_help(char **args);
 void findenv(char **cmds);
