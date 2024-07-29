316400006f504ca3b459d8e404c8612432ca0833
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-29 16:48:24 -0500

    Update builtins.c
    Testing this

--- builtins.c
+++ builtins.c
@@ -1,40 +1,11 @@
 #include "shell.h"
 
 /**
- * ss_cd - Changes the current working directory.
- * @cmds: The command to be executed.
- *
- * Return: 1 on success, -1 on failure.
- */
-int ss_cd(const char *cmds)
-{
-    if (!cmds || !cmds[1])
-    {
-        fprintf(stderr, "ss: expected argument to \"cd\"\n");
-        return (-1);
-    }
-    if (chdir(&cmds[1]) == -1)
-    {
-        perror("ss");
-        return (-1);
-    }
-    return (1);
-}
-
-/**
- * ss_exit - Exits the shell.
- */
-void ss_exit(void)
-{
-    exit(EXIT_SUCCESS);
-}
-
-/**
  * ss_help - Lists the built-in functions of the shell.
  */
 void ss_help(void)
 {
-    int i;
+    size_t i;
     const char *builtins[] = {"ss_cd", "ss_exit", "ss_help"};
 
     printf("Welcome to the ATLAS Simple Shell!\n");
